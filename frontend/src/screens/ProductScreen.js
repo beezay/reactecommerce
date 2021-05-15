22d6b007ea79c60d8d708fe9260df0fe072b2914
beezay bohora <bijay.okur@gmail.com>
2021-05-15 13:07:31 +0545

    develop branch started

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
-import products from "../products";
+
 import Rating from "../components/Rating";
 import { useEffect, useState } from "react";
 import axios from "axios";
