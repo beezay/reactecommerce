abc7846e6141032d0eac4099fd9b46a4b0b624fe
beezay bohora <bijay.okur@gmail.com>
2021-05-15 13:30:24 +0545

    ES6 Module done in serverjs

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,26 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const products = require('./data/products')
+import express from "express";
+import dotenv from "dotenv";
+import products from "./data/products.js";
 
-dotenv.config()
-const app = express()
+dotenv.config();
+const app = express();
 
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
-app.get('/', (req, res)=> {
-    res.send('API is running...')
-})
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((x) => x._id === req.params.id);
+  res.json(product);
+});
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(x => x._id === req.params.id)
-    res.json(product)
-})
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, console.log(`Server Running on ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(
+  PORT,
+  console.log(`Server Running on ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
