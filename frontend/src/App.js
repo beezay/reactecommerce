bcb342ca2e1ea7bb748556a384851615a63c594b
beezay bohora <bijay.okur@gmail.com>
2021-05-15 11:57:55 +0545

    router setup done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Container } from "react-bootstrap";
 import HomeScreen from "./screens/HomeScreen";
+import { Route } from "react-router-dom";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
   return (
@@ -9,7 +11,8 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Route exact path="/" component={HomeScreen} />
+          <Route path="/product/:id" component={ProductScreen} />
         </Container>
       </main>
       <Footer />
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import './bootstrap.min.css'
+import "./bootstrap.min.css";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
