558889ded8854b4943e257c5da15df09d292804c
beezay bohora <bijay.okur@gmail.com>
2021-05-24 12:26:08 +0545

    auth middleware created for token

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -4,7 +4,7 @@ import User from "../models/userModel.js";
 import generateToken from "../utils/generateToken.js";
 
 // @desc    Auth User and Get Token
-// @route   GET /api/users/login
+// @route   POST /api/users/login
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
@@ -25,4 +25,22 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser };
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not Found");
+  }
+});
+
+export { authUser, getUserProfile };
